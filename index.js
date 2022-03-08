4f1921b30b2690158dcfbdda135cbe2d562f114b
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2022-03-08 16:06:04 -0600

    gallery effect to top image

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-function toggle(nav) {
-    nav.classList.toggle('show');
+window.onload = function(){
+    const img = document.getElementById("topImg");
+
+    setInterval(imgChange, 5000);
+    let i = 0;
+    function imgChange(){
+        
+        const images = ["pizza1.jpg", "pizza2.jpg", "pizza4.jpg", "pizza6.jpg"];
+        if(i < 4){
+            img.src = "images/" + images[i];
+            i++;
+        }else{
+           i = 1;
+           img.src = "images/" + images[0];
+        };
+        
+    
+    }
+    
+    
+    
 }
