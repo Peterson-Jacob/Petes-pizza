827d74aebe0790622d88cb5e72067d9fee442a0b
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2022-03-09 16:03:14 -0600

    update styling and start on animation

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 window.onload = function(){
     const img = document.getElementById("topImg");
+    const one = document.getElementById("hOne");
 
-    setInterval(imgChange, 5000);
-    let i = 0;
+    
+    setInterval(imgChange, 3000);
+    let i = 1;
     function imgChange(){
-        
         const images = ["pizza1.jpg", "pizza2.jpg", "pizza4.jpg", "pizza6.jpg"];
         if(i < 4){
             img.src = "images/" + images[i];
@@ -13,10 +14,17 @@ window.onload = function(){
            i = 1;
            img.src = "images/" + images[0];
         };
-        
+    };
     
-    }
+    setInterval(fadeIn, 30);
+    let x = 0;
     
-    
+    function fadeIn(){
+
+        if( x < 1){
+            x += 0.02;
+            one.style.opacity = x;
+        };
+    };
     
 }
