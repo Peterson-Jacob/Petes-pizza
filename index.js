fefc0f638278c96dc39b38ba3d5b9d1446d99412
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2022-03-10 15:39:22 -0600

    update onload animation

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 window.onload = function(){
     const img = document.getElementById("topImg");
     const one = document.getElementById("hOne");
+    const two = document.getElementById("hTwo");
+    const bod = document.getElementById("mybody");
 
     
-    setInterval(imgChange, 3000);
+    setInterval(imgChange, 4000);
     let i = 1;
     function imgChange(){
         const images = ["pizza1.jpg", "pizza2.jpg", "pizza4.jpg", "pizza6.jpg"];
@@ -16,15 +18,30 @@ window.onload = function(){
         };
     };
     
-    setInterval(fadeIn, 30);
+    setInterval(fadeIn, 100);
     let x = 0;
-    
+    let y = 0;
+    let z = 0;
     function fadeIn(){
 
-        if( x < 1){
-            x += 0.02;
-            one.style.opacity = x;
+        if( x < 1 ){
+            x += 0.15;
+            bod.style.opacity = x;
+            //two.style.opacity = y;
+   
         };
+
+        if( bod.style.opacity >= 1){
+            
+            y += 0.05;
+            z += 0.04;
+            one.style.opacity = y;
+            two.style.opacity = z;
+        }
     };
+
+    
+    
+   
     
 }
