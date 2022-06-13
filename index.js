a590f0ebda831354ff1c0259ce5b9e68ccb2c8e1
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2022-06-13 15:26:50 -0500

    animated elements
    animated elements using jquery.

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 window.onload = function(){
     const img = document.getElementById("topImg");
-    const one = document.getElementById("hOne");
-    const two = document.getElementById("hTwo");
     const bod = document.getElementById("mybody");
 
     
-    setInterval(imgChange, 4000);
+    setInterval(imgChange, 5000);
     let i = 1;
     function imgChange(){
         const images = ["pizza1.jpg", "pizza2.jpg", "pizza4.jpg", "pizza6.jpg"];
@@ -17,31 +15,46 @@ window.onload = function(){
            img.src = "images/" + images[0];
         };
     };
-    
-    setInterval(fadeIn, 100);
-    let x = 0;
-    let y = 0;
-    let z = 0;
-    function fadeIn(){
+     
+    $(document).ready(function(){
+        const one = $("#hOne");
+        const two = $("#hTwo");
+        const side = $("#sides");
+        one.fadeIn(3000);
+        two.fadeIn(4000);
+      
 
-        if( x < 1 ){
-            x += 0.15;
-            bod.style.opacity = x;
-            //two.style.opacity = y;
+                $(document).scroll(function(){
+                    
+                
+                    if(document.documentElement.scrollTop > 75){
+                        $("#aboutHeader").fadeIn(3000); 
+                    };
+
+                    if(document.documentElement.scrollTop > 1800){
+        
+                        $("#menuHeader").fadeIn(3000);
+                       
+                    };
+
+                    if(document.documentElement.scrollTop > 2200){
+                     
+                        side.animate({
+                            left: '0%'
+                        });
+
+                        
+                    }
+                    
+                    
+                    
+                });
+
+                
+
+
    
-        };
 
-        if( bod.style.opacity >= 1){
-            
-            y += 0.05;
-            z += 0.04;
-            one.style.opacity = y;
-            two.style.opacity = z;
-        }
-    };
-
-    
-    
-   
+    });
     
 }
