3299cd2563d4f0caeec1a52e9959b1f8efeec857
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2023-05-09 12:01:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 window.onload = function(){
     const img = document.getElementById("topImg");
+    const quote = document.getElementById("proverb");
     const bod = document.getElementById("mybody");
 
     
-    setInterval(imgChange, 5000);
+    setInterval(imgChange, 4000);
     let i = 1;
     function imgChange(){
         const images = ["pizza1.jpg", "pizza2.jpg", "pizza4.jpg", "pizza6.jpg"];
+        const proverbs = ['“The perfect lover is one who turns into a pizza at 4:00 A.M.” – Charles Pierce' ,
+        '"Every pizza is a personal pizza if you try hard and believe in yourself." - Bill Murray',
+        '“Keep your friends close and your pizza closer.” – Anonymous',
+         '“Pizza is like the entire food pyramid!” – Madeline Oles']
         if(i < 4){
             img.src = "images/" + images[i];
+            quote.textContent = proverbs[i];
             i++;
         }else{
            i = 1;
            img.src = "images/" + images[0];
+           quote.textContent = proverbs[0];
         };
     };
      
+   
+   
     $(document).ready(function(){
         const one = $("#hOne");
         const two = $("#hTwo");
