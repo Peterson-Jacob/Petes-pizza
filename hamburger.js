556723623f18fa6654833508ec11252677153098
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2023-05-09 12:02:26 -0500

    Update hamburger.js

--- hamburger.js
+++ hamburger.js
@@ -1,3 +1,15 @@
+
+
+
 function toggle(nav) {
+    const hamburgerWidth = document.getElementById("hamburger-nav");
     nav.classList.toggle('show');
+    hamburgerWidth.style.width = "100%";
+
+    if(nav.classList == 'hamburger show'){
+        hamburgerWidth.style.width = "100%";
+    }else{
+        hamburgerWidth.style.width = "auto";
+    }
+    
 }
