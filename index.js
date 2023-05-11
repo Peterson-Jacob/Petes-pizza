332c3016b5d51656439dc4c577c009ba1b2ba0b0
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2023-05-11 11:12:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ window.onload = function(){
     const bod = document.getElementById("mybody");
 
     
-    setInterval(imgChange, 4000);
+    setInterval(imgChange, 5000);
     let i = 1;
     function imgChange(){
-        const images = ["pizza1.jpg", "pizza2.jpg", "pizza4.jpg", "pizza6.jpg"];
+        const images = ["pizza1.jpg", "pizza2.jpg", "pizza4.jpg", "pizza6.jpg", "pizza8.jpg", "pizza9.jpg", "pizza7.jpg"];
         const proverbs = ['“The perfect lover is one who turns into a pizza at 4:00 A.M.” – Charles Pierce' ,
         '"Every pizza is a personal pizza if you try hard and believe in yourself." - Bill Murray',
         '“Keep your friends close and your pizza closer.” – Anonymous',
-         '“Pizza is like the entire food pyramid!” – Madeline Oles']
-        if(i < 4){
+         '“Pizza is like the entire food pyramid!” – Madeline Oles',
+        '"Do good. Be nice. Order pizza. Repeat." - Anonymous"', 
+        '“Those pizzas I ate were for medicinal purposes.” – Amy Neftzger',
+        '“A pizza slice a day keeps sadness away.” – Jet Paacal']
+        if(i < 7){
             img.src = "images/" + images[i];
             quote.textContent = proverbs[i];
             i++;
@@ -25,7 +28,7 @@ window.onload = function(){
      
    
    
-    $(document).ready(function(){
+ /*    $(document).ready(function(){
         const one = $("#hOne");
         const two = $("#hTwo");
         const side = $("#sides");
@@ -115,6 +118,6 @@ window.onload = function(){
                     
                 });
 
-    });
+    }); */
     
 }
