eee0d35df7bf24745f2c71e7c610476c6954a795
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2023-07-11 16:01:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ window.onload = function(){
   
 
 
-    setInterval(imgChange, 3000);
+    setInterval(imgChange, 4000);
     let i = 1;
     function imgChange(){
         const images = ["pizza1.jpg", "pizza2.jpg", "pizza4.jpg", "pizza6.jpg", "pizza8.jpg", "pizza9.jpg", "pizza7.jpg"];
